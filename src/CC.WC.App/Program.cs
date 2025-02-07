5e89110c916ef2fb08cb5a1fe4d3e9e599ada56a
TheEclipse <TheEclipse@tut.by>
2025-02-07 23:19:28 +0100

    Read args, read std stream

--- src/CC.WC.App/Program.cs
+++ src/CC.WC.App/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace CC.WC.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach(var arg in args)
+            {
+                Console.WriteLine(arg);
+                Console.Beep();
+            }
+
+            Stream stdin = Console.OpenStandardInput();
+
+            byte[] buffer = new byte[10];
+
+            stdin.Read(buffer, 0, buffer.Length);
+
+            var output = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine($"Got from std: {output}");
+        }
+    }
+}
