1d5aa59347d31a2b9b1f18d55ead5cc107134cca
TheEclipse <TheEclipse@tut.by>
2025-02-08 22:31:54 +0100

    Step 2: count number of lines

--- src/CC.WC.App/Program.cs
+++ src/CC.WC.App/Program.cs
@@ -13,6 +13,9 @@ namespace CC.WC.App
                 case "-c":
                     System.Console.WriteLine(CountBytes(stdin));
                     break;
+                case "-l":
+                    System.Console.WriteLine(CountLines(stdin));
+                    break;
                 default:
                     System.Console.WriteLine(0);
                     break;
@@ -23,13 +26,38 @@ namespace CC.WC.App
         {
             int count = 0;
 
-            byte[] buffer = new byte[10];
+            byte[] buffer = new byte[1_000];
 
             int readedCount = 0;
             do
             {
                 readedCount = stream.Read(buffer, 0, buffer.Length);
                 count += readedCount;
+            }
+            while(readedCount != 0);
+
+            return count;
+        }
+
+        public static int CountLines(Stream stream)
+        {
+            int count = 0;
+
+            byte[] buffer = new byte[1_000];
+
+            int readedCount = 0;
+            do
+            {
+                readedCount = stream.Read(buffer, 0, buffer.Length);
+                
+                for(int i = 0; i < readedCount; i++)
+                {
+                    var c = (char)buffer[i];
+                    if(Char.IsControl(c) && c == '\n')//only unix handling
+                    {
+                        count++;
+                    }
+                }
             }
             while(readedCount != 0);
 
